6c98408cd0888aa8153c6e5b3afbf3613e3fb50d
coulibaly <aliscoul@yahoo.fr>
2023-05-31 17:25:43 +0200

    modification dossier src

--- src/components/section/banner/banner.jsx
+++ src/components/section/banner/banner.jsx
@@ -1,19 +1,18 @@
-import React from 'react'
+import React from 'react';
 import './banner.css';
-
 
 function Banner() {
   return (
     <section className='banner-container'> 
-      <h1 className= 'banner-text'>Chez vous, partout et ailleurs</h1>
+      <h1 className='banner-text'>
+        Chez vous <br className='responsive-line-break' /> partout et ailleurs
+      </h1>
       <div className='bannerImage'> 
-        <img src="./IMG.png" alt= 'banner' />
+        <img src="./IMG.png" alt='banner' />
         <p className='banner-overlay'></p>
       </div>
-
-     
     </section>
   );
 }
 
-export default Banner
+export default Banner;
